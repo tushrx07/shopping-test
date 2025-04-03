c24b92f7c9a4872d61ebaeb3386de7760b9408a2
tushrx07 <tushrrxx@gmail.com>
2025-04-03 17:06:28 +0530

    added colon

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import { BrowserRouter,Routes,Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Cart from './pages/Cart'
-import Navbar from './components/Navbar'
+import Navbar from './components/Navbar';
 
 
 
